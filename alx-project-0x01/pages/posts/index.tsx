f407a4b1e8981e76670feb57423ceb5234528cde
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-07-30 00:24:35 +0300

    Checked

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,44 @@
 import React from "react";
 import Header from "../../components/layout/Header";
-const Posts: React.FC = () => {
-  return <div></div>;
+import PostCard from "../../components/common/PostCard";
+import { PostProps, PostsPageProps } from "../../interfaces";
+
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Add Post
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+          {posts.map((post: PostProps) => (
+            <PostCard
+              key={post.id}
+              title={post.title}
+              body={post.body}
+              userId={post.userId}
+              id={post.id}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
 export default Posts;
