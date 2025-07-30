ee870a992653d458b29274da653358ac311c05d7
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-07-30 12:20:06 +0300

    Implemented PostModal.tsx component

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -33,3 +33,14 @@ export interface UserProps {
   geo: Geo;
   company: Company;
 }
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
